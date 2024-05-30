62d2029db7d1be1c9b990764be466a323751f919
maryamgithub0 <144210247+maryamgithub0@users.noreply.github.com>
2024-05-30 14:54:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-resource "aws_instance" "name" {
-    ami = "ami-00fa32593b478ad6e"
-    instance_type = "t2 micro"
-    key_name = "mykeypair"
-
-    tags = {
-      Name = "newtag"
-    }
-  
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "test" {
+  ami = "ami-00fa32593b478ad6e"
+  instance_type = "t2.micro"
+  key_name = "mykeypair"
+  tags = {
+    Name="test"
+  }
 }
